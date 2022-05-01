1534014ebd1d658d741618f1b31bdf3d815471b6
Катерина Попова <Kateryna.Popova.IT.2021@edu.lpnu.ua>
2022-05-01 17:55:54 +0300

    first commit

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\KATERYNA\Desktop\Лабораторні\Labb1.1\Labb1.1\CProgression.h"
+#include "C:\Users\KATERYNA\Desktop\Лабораторні\Labb1.1\Labb1.1\CProgression.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Progression pgr;
+			Assert::AreEqual(pgr.Init(1, 0), false);
+		}
+	};
+}
